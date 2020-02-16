07dc52e86e9821b8642081daae074d215a66b62d
Admin <vuongnguyenbinhh@gmail.com>
2020-02-16 16:57:41 +0700

    them index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+var NguoiDung = function(){
+    this.TaiKhoa = '';
+    this.HoTen = '';
+    this.Tuoi = '';
+    this.GioiTinh = '';
+    this.QuocTich ='';
+    this.giagiapha='';
+    this.mont = '';
+}
